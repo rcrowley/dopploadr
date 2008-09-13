02b6c6314f682ed7ef9f7338eaee35d907dd8928
Richard Crowley <r@rcrowley.org>
2008-09-13 02:29:03 -0700

    Proper geotagging using as much info as available.

--- content/overlay.js
+++ content/overlay.js
@@ -36,36 +36,50 @@ extension.after_logout.add(function() {
 // After a batch of photos is added, save the geo data for later
 //   TODO: Cache date -> geo data
 extension.after_thumb.add(function(id) {
+Components.utils.reportError('after_thumb! id: ' + id);
 	var d = photos.list[id].date_taken.match(/^(\d{4})[-:](\d{2})[-:](\d{2})/);
-	dopplr.location_on_date(d[1] + '-' + d[2] + '-' + d[3], function(loc) {
-		Components.utils.reportError(loc.toSource());
-		//photos.list[id].tags = meta.tags(photos.list[id].tags, tags);
-		var c = loc.location.trip ? loc.location.trip.city : loc.location.home;
-		photos.list[id].geo = {
-			lat: c.latitude,
-			lon: c.longitude,
-			tags: [
-				c.name.toLowerCase().replace(/\s/, ''),
-				'geonames:locality=' + c.geoname_id
-			]
-		};
-		if (loc.location.trip) {
-			photos.list[id].geo.tags.push(
-				'dopplr:trip=' + loc.location.trip_id);
+	dopplr.location_on_date(d[1] + '-' + d[2] + '-' + d[3], function(l) {
+Components.utils.reportError('location_on_date! id: ' + id);
+		Components.utils.reportError(l.toSource());
+		var c = l.location.trip ? l.location.trip.city : l.location.home;
+		var tags = [
+			c.name.toLowerCase().replace(/\s/g, ''),
+			'woe:id=' + c.woeid,
+			'geonames:locality=' + c.geoname_id
+		];
+		if (c.region) {
+			tags.push(c.region.toLowerCase().replace(/\s/g, ''));
 		}
-		Components.utils.reportError(photos.list[id].geo.toSource());
+		if (c.country) {
+			tags.push(c.country.toLowerCase().replace(/\s/g, ''));
+		}
+		if (l.location.trip) {
+			tags.push('dopplr:trip=' + l.location.trip.id);
+		}
+		photos.list[id].geo = {
+			'lat': c.latitude,
+			'lon': c.longitude,
+			'tags': tags
+		};
+		Components.utils.reportError(photos.list.toSource());
 	});
 });
 
 // Just before uploading a photo, add in the geo-related tags saved earlier
 extension.before_one_upload.add(function(photo) {
+Components.utils.reportError('before_one_upload! photo.id: ' + photo.id);
 	if (!photo.geo) { return; }
+Components.utils.reportError('before_one_upload! really');
 	photo.tags = meta.tags(photo.tags, photo.geo.tags.join(' '));
 });
 
 // Geotag photos as they successfully upload
 //   11 is the accuracy level for a city
 extension.after_one_upload.add(function(photo, success) {
+Components.utils.reportError('after_one_upload! photo.id: ' + photo.id +
+', success: ' + success);
 	if (!success || !photo.geo) { return; }
-	flickr.photos.geo.setLocation(null, users.token, photo.photo_id, photo.geo.lat, photo.geo.lon, 11);
+Components.utils.reportError('after_one_upload! really');
+	flickr.photos.geo.setLocation(null, users.token, photo.photo_id,
+		photo.geo.lat, photo.geo.lon, 11);
 });
